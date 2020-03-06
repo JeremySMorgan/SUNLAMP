7ae27dd6f1d356ec27df4c7d0b05920ad771a770
jeremy.morgan <jsmorgan6@gmail.com>
2020-03-05 20:42:35 -0800

    first commit

--- git_add.sh
+++ git_add.sh
@@ -5,3 +5,10 @@ git add git_add.sh
 git add data/simulation_test_worlds/\*.xml
 git add .gitignore
 git add data/point_cloud_data/\*.*
+
+git add git_add.sh
+
+git add data/simulation_test_worlds/objects/\*.obj
+git add data/simulation_test_worlds/objects/\*.off
+git add data/simulation_test_worlds/objects/\*.tri
+git add data/simulation_test_worlds/objects/\*.geom
